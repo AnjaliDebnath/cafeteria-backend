8b32de5faa2450c152b25631a10e1f0f6790ac53
AnjaliDebnath <anjalidebnath1003@gmail.com>
2025-01-28 15:20:31 +0530

    created routes for login and register

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,21 +14,13 @@ router.post("/register", async (req, res) => {
       const { name, password, email } = req.body;
       console.log(name, password, email);
   
-      // Validate input
       if (!name || !password || !email) {
         return res.status(400).json({ message: "All fields are required" });
       }
-  
-      // Hash the password
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(password, salt);
-  
-      // Create a new user instance
       const user = new User({ name, email, password: hashedPassword });
-  
-      // Save the user to the database
       await user.save();
-  
       res.status(201).json({ message: "User has been registered successfully" });
     } catch (err) {
       console.error(err);
@@ -40,26 +32,21 @@ router.post("/register", async (req, res) => {
     const { email, password } = req.body;
   
     try {
-      // Validate input
       if (!email || !password) {
         return res.status(400).json({ message: "Email and password are required" });
       }
-  
-      // Find user in MongoDB
       const user = await User.findOne({ email });
       if (!user) {
         return res.status(404).json({ message: "Invalid email or password" });
       }
   
-      // Compare passwords
       const isMatched = await bcrypt.compare(password, user.password);
       if (!isMatched) {
         return res.status(400).json({ message: "Invalid email or password" });
       }
   
-      // Generate tokens
-      const accessToken = generateToken({ user: user.name });
-      const refreshToken = jwt.sign({ user: user.name }, REFRESH_TOKEN_SECRET);
+      const accessToken = generateToken({ user: user.name, id : user._id });
+      const refreshToken = jwt.sign({ user: user.name, id :user._id }, REFRESH_TOKEN_SECRET);
    
       sessions.add(refreshToken);
       
